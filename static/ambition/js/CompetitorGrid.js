6e3dc9e701f76977484de7345c620e090f933883
Anthony M <antonius6k+github@gmail.com>
2014-04-14 13:16:27 -0500

    Fixed tabbing by replacing all leading tabs with 4 spaces.

--- static/ambition/js/CompetitorGrid.js
+++ static/ambition/js/CompetitorGrid.js
@@ -8,116 +8,116 @@
 // targetSelector => jQuery selector referring to the container for the grid
 //    minRowCount => the minimum number of rows that are rendered in the grid
 function CompetitorGrid(targetSelector, minRowCount) {
-	this.container = $(targetSelector);
-	this.minRows = minRowCount;
-	this.results = null;
-	this.lastSort = null;
+    this.container = $(targetSelector);
+    this.minRows = minRowCount;
+    this.results = null;
+    this.lastSort = null;
 }
 
 // define the properties of the grid 
 CompetitorGrid.prototype = {
-	// load the grid with API data
-	load: function() {
-		var self = this; // preserve scope
-		self.results = null;
+    // load the grid with API data
+    load: function() {
+        var self = this; // preserve scope
+        self.results = null;
 
-		$.ajax({
-			url: 'api/competitor',
-			data: {'firstName': 'Steve'},
-			dataType: 'json',
-			success: function(data) {
-				self.results = data;
-				self.render();
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				console.log('CompetitorGrid load: ', textStatus, '//', errorThrown);
-			}
-		});
-	},
+        $.ajax({
+            url: 'api/competitor',
+            data: {'firstName': 'Steve'},
+            dataType: 'json',
+            success: function(data) {
+                self.results = data;
+                self.render();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log('CompetitorGrid load: ', textStatus, '//', errorThrown);
+            }
+        });
+    },
 
-	// render the grid with the loaded data
-	render: function() {
-		var self = this; // preserve scope
+    // render the grid with the loaded data
+    render: function() {
+        var self = this; // preserve scope
 
-		// only render the grid if there were objects in the results
-		if (self.results) {
-			var count = self.results.meta.count,
-				total = self.results.meta.total,
-				tableItems = '';
+        // only render the grid if there were objects in the results
+        if (self.results) {
+            var count = self.results.meta.count,
+                total = self.results.meta.total,
+                      tableItems = '';
 
-			// add rows
-			$.each(self.results.objects, function(index, item) {
-				tableItems += '<tr id="' + item._id + '">';
-				tableItems += '<td>' + item.user.email + '</td>';
-				tableItems += '<td>' + item.firstName + ' ' + item.lastName + '</td>';
-				tableItems += '<td>' + item.teamName + '</td>';
-				tableItems += '<td>' + item.score + '</td>';
-			});
+            // add rows
+            $.each(self.results.objects, function(index, item) {
+                tableItems += '<tr id="' + item._id + '">';
+                tableItems += '<td>' + item.user.email + '</td>';
+                tableItems += '<td>' + item.firstName + ' ' + item.lastName + '</td>';
+                tableItems += '<td>' + item.teamName + '</td>';
+                tableItems += '<td>' + item.score + '</td>';
+            });
 
-			// add enough rows to get up to 10 rows total
-			for (var i = count; i < self.minRows; i++) {
-				tableItems += '<tr><td></td><td></td><td></td><td></td></tr>';
-			}
+            // add enough rows to get up to 10 rows total
+            for (var i = count; i < self.minRows; i++) {
+                tableItems += '<tr><td></td><td></td><td></td><td></td></tr>';
+            }
 
-			// and change the summary text
-			tableItems += '<tr><td class="grid-summary" colspan="4">';
-			tableItems += 'Showing ' + count + ' of ' + total;
-			tableItems += '</td></tr>';
+            // and change the summary text
+            tableItems += '<tr><td class="grid-summary" colspan="4">';
+            tableItems += 'Showing ' + count + ' of ' + total;
+            tableItems += '</td></tr>';
 
-			// render it to the grid body
-			self.container.find('.grid-body').html(tableItems);
-		}
-	},
+            // render it to the grid body
+            self.container.find('.grid-body').html(tableItems);
+        }
+    },
 
-	// add the sort handlers to the table headings
-	addSort: function() {
-		var self = this; // preserve scope
+    // add the sort handlers to the table headings
+    addSort: function() {
+        var self = this; // preserve scope
 
-		// add a handler for each heading based on its index;
-		// this way allows the position of the heading to correspond to
-		// a comparison function in the CG.sorters table
-		self.container.find('.grid-heading > th').each(function(index) {
-			$(this).on('click', function() {
-				if (self.lastSort === index) {
-					// if we've already sorted on this column, then reverse it
-					self.results.objects.reverse();
-				} else {
-					// otherwise, do a new sort
-					var key = $(this).attr('data-sortkey');
-					self.results.objects.sort(self.sorters[key]);
-				}
+        // add a handler for each heading based on its index;
+        // this way allows the position of the heading to correspond to
+        // a comparison function in the CG.sorters table
+        self.container.find('.grid-heading > th').each(function(index) {
+            $(this).on('click', function() {
+                if (self.lastSort === index) {
+                    // if we've already sorted on this column, then reverse it
+                    self.results.objects.reverse();
+                } else {
+                    // otherwise, do a new sort
+                    var key = $(this).attr('data-sortkey');
+                    self.results.objects.sort(self.sorters[key]);
+                }
 
-				// remember the last heading that we sorted on
-				self.lastSort = index;
+                // remember the last heading that we sorted on
+                self.lastSort = index;
 
-				// rerender the grid 
-				self.render();
-			});
-		});
-	},
+                // rerender the grid 
+                self.render();
+            });
+        });
+    },
 
-	// return a sort function based on an object property
-	// based on an idea from:
-	// http://stackoverflow.com/questions/2466356/javascript-object-list-sorting-by-object-property 
-	sorters: {
-		'email': function(a,b) {
-			return ((a.user.email < b.user.email) ? -1 : ((a.user.email > b.user.email) ? 1 : 0));
-		},
-		'name': function(a,b) {
-			return ((a.lastName < b.lastName) ? -1 : ((a.lastName > b.lastName) ? 1 : 0));
-		},
-		'team': function(a,b) {
-			return ((a.teamName < b.teamName) ? -1 : ((a.teamName > b.teamName) ? 1 : 0));
-		},
-		'score': function(a,b) {
-			return (a.score - b.score);
-		}
-	}
+    // return a sort function based on an object property
+    // based on an idea from:
+    // http://stackoverflow.com/questions/2466356/javascript-object-list-sorting-by-object-property 
+    sorters: {
+        'email': function(a,b) {
+            return ((a.user.email < b.user.email) ? -1 : ((a.user.email > b.user.email) ? 1 : 0));
+        },
+        'name': function(a,b) {
+            return ((a.lastName < b.lastName) ? -1 : ((a.lastName > b.lastName) ? 1 : 0));
+        },
+        'team': function(a,b) {
+            return ((a.teamName < b.teamName) ? -1 : ((a.teamName > b.teamName) ? 1 : 0));
+        },
+        'score': function(a,b) {
+            return (a.score - b.score);
+        }
+    }
 };
 
 // ready function -- execute when page loads
 $(function() {
-	var cg = new CompetitorGrid('#competitor-grid', 10);
-	cg.load();
-	cg.addSort();
+    var cg = new CompetitorGrid('#competitor-grid', 10);
+    cg.load();
+    cg.addSort();
 });
