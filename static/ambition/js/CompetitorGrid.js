81a3ede37b72cf64886d25482262e1eea9d2a050
Anthony M <antonius6k+github@gmail.com>
2014-04-14 13:08:25 -0500

    Completely refactored CompetitorGrid.js - Made CompetitorGrid a proper class with a constructor, properties, and   methods - Fixed readability issues by using self instead of _this for   maintaining scope in the class - Changed sorters to be a dictionary of methods with keys corresponding   to the data-sortkey property on headings in index.html - Added minRows property for declaring the minimum grid size - Added container property for the grid (and made all relevant elements   have classes based off of this container instead of having unique id   attributes for each of them) - Modified index.html and ambition-all.css to coincide with these   changes

--- static/ambition/js/CompetitorGrid.js
+++ static/ambition/js/CompetitorGrid.js
@@ -3,108 +3,121 @@
  * CompetitorGrid should be an object that can render a sortable grid.
  */
 
-var CompetitorGrid = {};
+// constructor for the CompetitorGrid object
+// parameters:
+// targetSelector => jQuery selector referring to the container for the grid
+//    minRowCount => the minimum number of rows that are rendered in the grid
+function CompetitorGrid(targetSelector, minRowCount) {
+	this.container = $(targetSelector);
+	this.minRows = minRowCount;
+	this.results = null;
+	this.lastSort = null;
+}
 
-// load the grid with API data
-CompetitorGrid.load = function() {
-	var _this = this; // preserve scope
-	_this.results = {};
+// define the properties of the grid 
+CompetitorGrid.prototype = {
+	// load the grid with API data
+	load: function() {
+		var self = this; // preserve scope
+		self.results = null;
 
-	$.ajax({
-		url: 'api/competitor',
-		data: {'firstName': 'Steve'},
-		dataType: 'json',
-		success: function(data) {
-			_this.results = data;
-			_this.lastSort = -1;
-			_this.render();
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			console.log('CompetitorGrid load: ', textStatus, '//', errorThrown);
-		}
-	});
-};
-
-// render the grid with the loaded data
-CompetitorGrid.render = function() {
-	var _this = this; // preserve scope
-
-	// only render things if there were objects in the list
-	if (this.results !== {}) {
-		var count = _this.results.meta.count,
-			total = _this.results.meta.total,
-			tableItems = '';
-
-		// add rows
-		$.each(_this.results.objects, function(index, item) {
-			tableItems += '<tr><td>' + item.user.email + '</td>';
-			tableItems += '<td>' + item.firstName + ' ' + item.lastName + '</td>';
-			tableItems += '<td>' + item.teamName + '</td>';
-			tableItems += '<td>' + item.score + '</td>';
+		$.ajax({
+			url: 'api/competitor',
+			data: {'firstName': 'Steve'},
+			dataType: 'json',
+			success: function(data) {
+				self.results = data;
+				self.render();
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log('CompetitorGrid load: ', textStatus, '//', errorThrown);
+			}
 		});
+	},
 
-		// add enough rows to get up to 10 rows total
-		for (var i = count; i < 10; i++) {
-			tableItems += '<tr><td></td><td></td><td></td><td></td></tr>';
-		}
+	// render the grid with the loaded data
+	render: function() {
+		var self = this; // preserve scope
 
-		// and change the summary text
-		tableItems += '<tr><td id=\'competitor-table-summary\' colspan=\'4\'>';
-		tableItems += 'Showing ' + count + ' of ' + total;
-		tableItems += '</td></tr>';
+		// only render the grid if there were objects in the results
+		if (self.results) {
+			var count = self.results.meta.count,
+				total = self.results.meta.total,
+				tableItems = '';
 
-		// render it
-		$('#competitor-table-body').html(tableItems);
-	}
-};
+			// add rows
+			$.each(self.results.objects, function(index, item) {
+				tableItems += '<tr id="' + item._id + '">';
+				tableItems += '<td>' + item.user.email + '</td>';
+				tableItems += '<td>' + item.firstName + ' ' + item.lastName + '</td>';
+				tableItems += '<td>' + item.teamName + '</td>';
+				tableItems += '<td>' + item.score + '</td>';
+			});
 
-// add the sort handlers to the table headings
-CompetitorGrid.addSort = function() {
-	var _this = this; // preserve scope
-
-	// add a handler for each heading based on its index;
-	// this way allows the position of the heading to correspond to
-	// a comparison function in the CG.sorters table
-	$('#competitor-table-heading th').each(function(index) {
-		$(this).on('click', function() {
-			if (_this.lastSort == index) {
-				// if we've already sorted on this column, then reverse it
-				_this.results.objects.reverse();
-			} else {
-				// otherwise, do a new sort
-				_this.results.objects.sort(_this.sorters[index]);
+			// add enough rows to get up to 10 rows total
+			for (var i = count; i < self.minRows; i++) {
+				tableItems += '<tr><td></td><td></td><td></td><td></td></tr>';
 			}
 
-			// remember the last sort
-			_this.lastSort = index;
+			// and change the summary text
+			tableItems += '<tr><td class="grid-summary" colspan="4">';
+			tableItems += 'Showing ' + count + ' of ' + total;
+			tableItems += '</td></tr>';
 
-			// rerender the table
-			_this.render();
+			// render it to the grid body
+			self.container.find('.grid-body').html(tableItems);
+		}
+	},
+
+	// add the sort handlers to the table headings
+	addSort: function() {
+		var self = this; // preserve scope
+
+		// add a handler for each heading based on its index;
+		// this way allows the position of the heading to correspond to
+		// a comparison function in the CG.sorters table
+		self.container.find('.grid-heading > th').each(function(index) {
+			$(this).on('click', function() {
+				if (self.lastSort === index) {
+					// if we've already sorted on this column, then reverse it
+					self.results.objects.reverse();
+				} else {
+					// otherwise, do a new sort
+					var key = $(this).attr('data-sortkey');
+					self.results.objects.sort(self.sorters[key]);
+				}
+
+				// remember the last heading that we sorted on
+				self.lastSort = index;
+
+				// rerender the grid 
+				self.render();
+			});
 		});
-	});
-};
+	},
 
-// return a sort function based on an object property
-// source: http://stackoverflow.com/questions/2466356/javascript-object-list-sorting-by-object-property 
-// array corresponds to the headings layout
-// [email, name, team, score]
-CompetitorGrid.sorters = [
-	function(a,b) {
-		return ((a.user.email < b.user.email) ? -1 : ((a.user.email > b.user.email) ? 1 : 0));
-	},
-	function(a,b) {
-		return ((a.lastName < b.lastName) ? -1 : ((a.lastName > b.lastName) ? 1 : 0));
-	},
-	function(a,b) {
-		return ((a.teamName < b.teamName) ? -1 : ((a.teamName > b.teamName) ? 1 : 0));
-	},
-	function(a,b) {
-		return (a.score - b.score);
+	// return a sort function based on an object property
+	// based on an idea from:
+	// http://stackoverflow.com/questions/2466356/javascript-object-list-sorting-by-object-property 
+	sorters: {
+		'email': function(a,b) {
+			return ((a.user.email < b.user.email) ? -1 : ((a.user.email > b.user.email) ? 1 : 0));
+		},
+		'name': function(a,b) {
+			return ((a.lastName < b.lastName) ? -1 : ((a.lastName > b.lastName) ? 1 : 0));
+		},
+		'team': function(a,b) {
+			return ((a.teamName < b.teamName) ? -1 : ((a.teamName > b.teamName) ? 1 : 0));
+		},
+		'score': function(a,b) {
+			return (a.score - b.score);
+		}
 	}
-];
+};
 
 // ready function -- execute when page loads
 $(function() {
-	CompetitorGrid.load(); // this will do the render
-	CompetitorGrid.addSort();
+	var cg = new CompetitorGrid('#competitor-grid', 10);
+	cg.load();
+	cg.addSort();
 });
